f00ac360fb94f9bb4e943954c9af908b14b922ad
seemakus <saraswathi.seemakurthi@zimmerbiomet.com>
2024-06-27 16:48:22 -0500

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by seemakus on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
